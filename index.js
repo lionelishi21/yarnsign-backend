f59efeb2aadd86bc3144eb55f1f0de5cd1daa645
Lionel Francis <lionelishmael@gmail.com>
2025-08-12 05:42:47 -0500

    update allow originn

--- index.js
+++ index.js
@@ -52,7 +52,8 @@ app.use((req, res, next) => {
     "http://localhost:5175", 
     "http://localhost:5174",
     "http://localhost:5173",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://yaadsign.com"
   ];
   
   const origin = req.headers.origin;
